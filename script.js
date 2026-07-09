636de8e3e81bd23b40213be6bf893d32602a31e5
prateekrajsingh22 <129385202+prateekrajsingh22@users.noreply.github.com>
2026-07-09 19:20:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         step4: document.getElementById('step4'),
     };
 
-    const recipientName = "Sapthesh"; // Customizable Name
+    const recipientName = "Diksha"; // Customizable Name
     const messageGreeting = "Happy Birthday,"; // Customizable Greeting
 
     // --- Utility Function to Transition Steps ---
